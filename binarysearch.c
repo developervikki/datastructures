a450c2295331699cfbb7179d79881b23c6499049
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-03-24 22:06:31 +0530

    Binary search in Data Structures using C.
    Binary search in Data Structures using C.

--- binarysearch.c
+++ binarysearch.c
@@ -3,11 +3,11 @@
 int binarySearch(int arr[], int size, int target) {
     int left = 0;
     int right = size - 1;
-      
+    
     while (left <= right) {
-        int mid = left + (right - left) / 2; 
+        int mid = left + (right - left) / 2;
         
-        if (arr[mid] == target) 
+        if (arr[mid] == target)
             return mid;
         
         if (arr[mid] < target)
