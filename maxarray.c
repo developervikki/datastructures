d3a72633503cd133ce111829735e5f52fab7b3d7
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-04-04 22:21:46 +0530

    A program to find maximum in array
    A program to find maximum in array

---
+++ maxarray.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i,arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int sum = 0;
+
+    for(i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    printf("The sum of all elements in the array is %d\n", sum);
+
+    return 0;
+}
