cf3ebfa0b82ca5d0f59fe9a17fb3556ee6d9e839
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2024-05-01 09:35:09 +0530

    Update binarysearch.c

--- binarysearch.c
+++ binarysearch.c
@@ -7,7 +7,7 @@ int binarySearch(int arr[], int size, int target) {
     while (left <= right) {
         int mid = left + (right - left) / 2;
         
-        if (arr[mid] == target)
+        if (arr[mid] == target) 
             return mid;
         
         if (arr[mid] < target)
